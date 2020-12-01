66fd2e43c91d5bfcaa1503ada6d5650025104cfb
hch <617320696@qq.com>
2020-12-01 22:49:36 +0800

    feat(c):增加指针结构体demo

--- blog-c-demo/basic/ptr.c
+++ blog-c-demo/basic/ptr.c
@@ -6,19 +6,36 @@ void arr_ptr()
     char *p;
     printf("%x, %p\n", p, p);
     p = malloc(sizeof(char));
-    printf("%x, %p, size: %d\n", p, p, sizeof(p));
+    *p = 255;
+    printf("%#x, %x, %p, size: %d\n", *p, p, p, sizeof(p));
 
-    int arr[]={};
+    int arr[] = {};
     for (int i = 0; i < 10; i++)
     {
         printf("%x ", arr[i]);
     }
-    printf(" size: %d\n", sizeof(arr));
+    printf(" size: %lu\n", sizeof(arr));
 
     printf("%x, %x, %p, %p\n", arr, &*arr, arr, &arr[0]);
+}
+
+typedef struct person
+{
+    int age;
+    char *name;
+} person_t;
+
+void struct_ptr()
+{
+    person_t p = {18, "hch"};
+    person_t *p1 = malloc(sizeof(person_t));
+    p1->age = 20;
+    p1->name = "abaaba";
+    printf("{%d, %s} {%d, %s}\n", p.age, p.name, (*p1).age, p1->name);
 }
 
 int main()
 {
     arr_ptr();
+    struct_ptr();
 }
